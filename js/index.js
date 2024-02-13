cc982e90335db39b5b93a276c01dd9f079cbfc26
lucas sosa <sosalucas906@gmail.com>
2024-02-13 00:27:08 -0300

    hoja de ruta por fecha

--- js/index.js
+++ js/index.js
@@ -4,7 +4,6 @@ let tableContainer = document.querySelector('.tableBody')
 
 const form = document.querySelector('.formField')
 let formInputs = document.getElementsByClassName('form-input')
-console.log(formInputs)
 let inputsGuardados = JSON.parse(localStorage.getItem('datosNuevaFila'))
 let formInputsArray = inputsGuardados ? inputsGuardados : []
 
@@ -18,6 +17,7 @@ class Row {
         this.bultos = bultos
         this.lluvia = lluvia
         this.fuera = fuera
+        this.date = date
     }
 }
 
@@ -29,10 +29,13 @@ let demora
 let bultos  
 let lluvia 
 let fuera 
+let date
+
+let fecha = document.getElementById('fecha')
+let newDate = localStorage.getItem('dateTime')
+fecha.value = newDate
 
 form.addEventListener('submit', (evt) => {
-    evt.preventDefault()
-    ++contador
     cliente = form.validationServer01.value
     destino = form.validationServer02.value
     solicitante = form.validationServerUsername.value
@@ -41,24 +44,12 @@ form.addEventListener('submit', (evt) => {
     bultos = form.validationServer05.value
     lluvia = form.invalidCheck3.checked
     fuera = form.invalidCheck4.checked
-    let nuevaFila = new Row(cliente, destino, solicitante, unidades, demora,bultos, lluvia, fuera)
+    date = fecha.value
+
+    let nuevaFila = new Row(cliente, destino, solicitante, unidades, demora,bultos, lluvia, fuera, date)
     formInputsArray.push(nuevaFila)
+    localStorage.setItem('dateTime', date)
     localStorage.setItem('datosNuevaFila', JSON.stringify(formInputsArray))
-
-    tableContainer.innerHTML +=`
-        
-            <td>${contador}</td>
-            <td>${cliente}</td>
-            <td>${destino}</td>
-            <td>${solicitante}</td>
-            <td>${unidades}</td>
-            <td>${demora}</td>
-            <td>${bultos}</td>
-            <td>${lluvia}</td>
-            <td>${fuera}</td>
-        
-    `
-
     // for (let i= 0; i < formInputs.length; i++) {
     //     formInputsArray.push(formInputs[i].value)
     //     localStorage.setItem('datosNuevaFila', JSON.stringify(formInputs[i].value))
@@ -67,66 +58,37 @@ form.addEventListener('submit', (evt) => {
     //     formInputsArray.push(formInputs[input].value)
     // }
 })
-let newRow = JSON.parse(localStorage.getItem('datosNuevaFila'))
+renderRow()
+
+let dateSubmit = document.querySelector('.dateForm')
+
+
+dateSubmit.addEventListener('submit', ()=> {
+    date = fecha.value
+    localStorage.setItem('dateTime', date)
+    renderRow()
+})
+
 function renderRow() {
+    let newRow = JSON.parse(localStorage.getItem('datosNuevaFila'))
 
+    // newRow.forEach(roww => {
+        
+    // });
+    for (let i = 0; i < newRow.length; i++) {
+        // let obj[i] = newRow[i];
+        if (newDate == newRow[i].date) {
+            tableContainer.innerHTML +=`
+            <td>${++contador}</td>
+            <td>${newRow[i].cliente}</td>
+            <td>${newRow[i].destino}</td>
+            <td>${newRow[i].solicitante}</td>
+            <td>${newRow[i].unidades}</td>
+            <td>${newRow[i].demora}</td>
+            <td>${newRow[i].bultos}</td>
+            <td>${newRow[i].lluvia}</td>
+            <td>${newRow[i].fuera}</td>
+            `
+        }
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // let newTableRow = tableContainer.insertRow(-1)
-    // let newTableRowRef = newTableRow.insertCell(0)
-    // newTableRowRef.textContent = JSON.parse(localStorage.getItem('cont'))
-    // localStorage.setItem('cont', JSON.stringify(contador))
-    // newTableRowRef = newTableRow.insertCell(1)
-    // newTableRowRef.textContent = form.validationServer01.value
-    // newTableRowRef = newTableRow.insertCell(2)
-    // newTableRowRef.textContent = form.validationServer02.value
-    // newTableRowRef = newTableRow.insertCell(3)
-    // newTableRowRef.textContent = form.validationServerUsername.value
-    // newTableRowRef = newTableRow.insertCell(4)
-    // newTableRowRef.textContent = form.validationServer03.value
-    // newTableRowRef = newTableRow.insertCell(5)
-    // newTableRowRef.textContent = form.validationServer04.value
-    // newTableRowRef = newTableRow.insertCell(6)
-    // newTableRowRef.textContent = form.validationServer05.value
-    // newTableRowRef = newTableRow.insertCell(7)
-    // if (invalidCheck3.checked) {
-    //     form.invalidCheck3.value = 'Sí'
-    //     newTableRowRef.textContent = form.invalidCheck3.value
-    // } else {
-    //     form.invalidCheck3.value = 'No'
-    //     newTableRowRef.textContent = form.invalidCheck3.value
-    // }
-    // newTableRowRef = newTableRow.insertCell(8)
-    // newTableRowRef.textContent = form.invalidCheck4.value
-    // if (invalidCheck4.checked) {
-    //     form.invalidCheck4.value = 'Sí'
-    //     newTableRowRef.textContent = form.invalidCheck4.value
-    // } else {
-    //     form.invalidCheck4.value = 'No'
-    //     newTableRowRef.textContent = form.invalidCheck4.value
-    // }
-
